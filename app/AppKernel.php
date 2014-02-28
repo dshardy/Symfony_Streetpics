cbf8659a8dc56030b278822453759e0d8dce8ec9
Darren Hardy <darren_hardy@icloud.com>
2014-02-28 01:53:01 +0000

    At least it's not crashing with css, twig, ascetic edited

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
-
-
             new Dsh\streetpicsBundle\DshstreetpicsBundle(),
         );
 
--- src/Dsh/streetpicsBundle/Controller/DefaultController.php
+++ src/Dsh/streetpicsBundle/Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace Dsh\streetpicsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($name)
     {
-        return $this->render('DshstreetpicsBundle:Default:index.html.twig');
+       
+         $content = $this->renderView(
+        'DshstreetpicsBundle:Default:index.html.twig',
+         array('name' => $name)
+         );
+
+         return new Response($content);
+        
+        
+        
     }
 }
+
+
+
+
